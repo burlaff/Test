e6344470a147ea4dda715347f4f12db448a9a5b1
Chris <w16038628@northumbria.ac.uk>
2023-10-23 23:18:54 +0100

    New

--- js/functions.js
+++ js/functions.js
@@ -37,9 +37,9 @@ function generateTable() {
         // 10K Time Column
         column += '<td>' + calculateTime(6.2, MPHValue) + '</td>';
         // Half Marathon Time Column
-        column += '<td>' + calculateTime(13.1, MPHValue) + '</td>';
+        column += '<td>' + formatHours(calculateTime(13.1, MPHValue)) + '</td>';
         // Marathon Time Column
-        column += '<td>' + calculateTime(26.2, MPHValue) + '</td>';
+        column += '<td>' + formatHours(calculateTime(26.2, MPHValue)) + '</td>';
 
         // Add the column to the row
         newRow.append(column);
@@ -54,6 +54,7 @@ function generateTable() {
 
 }
 
+// Convert MPH to minutes per mile
 function convertToMinutesPerMile(MPHValue) {
 
     // Variables
@@ -61,7 +62,7 @@ function convertToMinutesPerMile(MPHValue) {
     let secondsOnly;
     let minsOnly;
 
-    // Calcaulte minutes per mile from MPH value
+    // Calculate minutes per mile from MPH value
     minsPerMile = 60 / MPHValue;
     secondsOnly = minsPerMile % 1;
     secondsOnly = secondsOnly * 60;
@@ -80,6 +81,7 @@ function convertToMinutesPerMile(MPHValue) {
 
 }
 
+// Add leading zero to value
 function addLeadingZero(numberToCheck) {
     if (numberToCheck.toString().length === 1) {
         numberToCheck = "0" + numberToCheck;
@@ -88,6 +90,7 @@ function addLeadingZero(numberToCheck) {
     return numberToCheck;
 }
 
+// Add .0 to end of value
 function addDecimalPlace(numberToRound) {
     if (numberToRound.toString().length === 1) {
         numberToRound = numberToRound.toString() + "." + "0";
@@ -96,6 +99,7 @@ function addDecimalPlace(numberToRound) {
     return numberToRound;
 }
 
+// Calculate time from distance and speed
 function calculateTime(distance, speed) {
 
     // Calculate time in hours, * 60 to convert to mins
@@ -116,3 +120,30 @@ function calculateTime(distance, speed) {
 
 }
 
+function formatHours(timeToFormat) {
+
+    // Variables
+    let timeInHours;
+    let remainingMinutes;
+    let remainingSeconds;
+
+    // If time is greater than 60 minutes
+    if (timeToFormat >= 60) {
+        // Calculate how many hours
+        timeInHours = timeToFormat / 60;
+
+        // Get remaining minutes from that value
+        remainingMinutes = timeInHours % 1;
+        remainingMinutes = remainingMinutes * 60;
+
+        remainingSeconds = remainingMinutes % 1;
+        remainingSeconds = remainingSeconds * 60;
+
+        timeInHours = Math.floor(timeInHours);
+
+        return timeInHours + ":" + remainingMinutes + ":" + remainingSeconds;
+
+    }
+
+
+}
