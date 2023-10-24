7120b5de355e4e28d26cff39fb8ccab31aede3a0
Chris <w16038628@northumbria.ac.uk>
2023-10-24 21:31:12 +0100

    New

--- js/functions.js
+++ js/functions.js
@@ -19,17 +19,17 @@ function generateTable() {
 
         // ---- Add the values to the columns ---- ///
         // MPH Column
-        column += '<td>' + MPHValueFormatted + '</td>';
+        column += '<td class="tableRow">' + MPHValueFormatted + '</td>';
         // Mins per mile column
-        column += '<td>' + convertToMinutesPerMile(MPHValue) + '</td>';
+        column += '<td class="tableRow>' + convertToMinutesPerMile(MPHValue) + '</td>';
         // 5K Time Column
-        column += '<td>' + calculateTime(3.1, MPHValue) + '</td>';
+        column += '<td class="tableRow>' + calculateTime(3.1, MPHValue) + '</td>';
         // 10K Time Column
-        column += '<td>' + formatHours(calculateTime(6.2, MPHValue)) + '</td>';
+        column += '<td class="tableRow>' + formatHours(calculateTime(6.2, MPHValue)) + '</td>';
         // Half Marathon Time Column
-        column += '<td>' + formatHours(calculateTime(13.1, MPHValue)) + '</td>';
+        column += '<td class="tableRow>' + formatHours(calculateTime(13.1, MPHValue)) + '</td>';
         // Marathon Time Column
-        column += '<td>' + formatHours(calculateTime(26.2, MPHValue)) + '</td>';
+        column += '<td class="tableRow>' + formatHours(calculateTime(26.2, MPHValue)) + '</td>';
 
         // Add the column to the row
         newRow.append(column);
@@ -117,7 +117,6 @@ function formatHours(timeToFormat) {
     timeToFormat = timeToFormat.replace(":", ".");
     timeToFormat = parseFloat(timeToFormat);
 
-    console.log("Time to format after parse: " + timeToFormat);
 
     // Variables
     let timeInHours;
